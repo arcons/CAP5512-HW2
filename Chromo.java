f96c25d135b3a93341fa9948b00b956823c1fd08
Mountbeach <mastermountbeach@yahoo.com>
2016-02-23 22:20:36 -0500

    Added files via upload

--- Chromo.java
+++ Chromo.java
@@ -1,12 +1,13 @@
+
 /******************************************************************************
-*  Based on Chromo.java: 
+*  Based on Chromo.java:
 *  A Teaching GA					  Developed by Hal Stringer & Annie Wu, UCF
 *  Version 2, January 18, 2004
-*  
-*  
+*
+*
 *  SequentialChromo.java 	          by Brandon Jones
 *  February 18, 2016
-*  
+*
 *  Added Integer and Float chromosome representations
 *******************************************************************************/
 
@@ -22,6 +23,7 @@ public class Chromo
 *******************************************************************************/
 
 	public List<Integer> chromo;
+    public ArrayList <Float> keys;
 	public double rawFitness;
 	public double sclFitness;
 	public double proFitness;
@@ -51,31 +53,31 @@ public class Chromo
 /*******************************************************************************
 *                                MEMBER METHODS                                *
 *******************************************************************************/
-	
+
 	// Retrieve a Persons X Shifts matrix, indicating what time is assigned
 	// to each person for each of there shifts
-	
+
 	public int[][] getTimeRepresentation(){
 		// TODO: corresponds to getRandomTimeChromosome
 		int[][] timeSlots = new int[7][5];
-		
+
 		for (int i = 0; i < Parameters.geneSize; ++i){
 			int shift = i % 5;
 			int person = i / 5;
 			timeSlots[person][shift] = this.chromo.get(i);
 		}
-		
+
 		return timeSlots;
 	}
-	
+
 	public int [][] getPersonRepresentation(){
 		// TODO: corresponds to getRandomPersonChromosome
 		return null;
 	}
-	
+
 	// Generate a string representing a new, random chromosome
 	// Values represent which time slots (1-35); P1S1, P1S2... P7S5
-	
+
 	public void getRandomTimeChromosome(){
 		chromo = new ArrayList<Integer>();
 		for (int i = 1; i <= Parameters.geneSize; ++i){
@@ -83,10 +85,10 @@ public class Chromo
 		}
 		Collections.shuffle(chromo, Search.r);
 	}
-	
+
 	// Generate a string representing a new, random chromosome
 	// Values represent people (1-7); D1S1, D1S2 ... D7S5
-	
+
 	public void getRandomPersonChromosome(){
 		chromo = new ArrayList<Integer>();
 		for (int i = 0; i < Parameters.geneSize; ++i){
@@ -94,11 +96,45 @@ public class Chromo
 		}
 		Collections.shuffle(chromo, Search.r);
 	}
-	
+
+
+	// Generate a string representing a new, random chromosome and a corresponding key
+	// Values in the key array correspond to people by taking the index mod 7, i.e. the first 7 slots represent person 1
+	// Values in chromo represent people (1-7); D1S1, D1S2 ... D7S5
+    public void getRandomKeyRepresentation(){
+        chromo = new ArrayList<Integer>();
+        keys = new ArrayList<Float>();
+
+		for (int i = 0; i < Parameters.geneSize; ++i){
+
+            keys.add(Search.r.nextFloat());
+
+		}
+
+		sortKeys();
+
+	}
+    
+        // Sorts chromo array based on keys
+    public void sortKeys(){
+        ArrayList<Float> sorted = new ArrayList<Float>(keys);
+        
+		Collections.sort(sorted);
+
+		chromo.clear();
+
+		for (int i = 0; i < Parameters.geneSize; ++i){
+			int shift = i % 5;
+			int person = i / 7;
+			chromo.add(sorted.indexOf(keys.get(i))/7);
+		}
+
+	}
 
 	//  Mutate a Chromosome Based on Mutation Type *****************************
 
 	public void doMutation(){
+
 
 		switch (Parameters.mutationType){
 
@@ -112,11 +148,20 @@ public class Chromo
 			}
 			break;
 
-		default:
+        case 2:     //Random Key sort - by Austen
+
+            for (int j=0; j<(Parameters.geneSize); j++){
+               randnum = Search.r.nextDouble();
+               if (randnum < Parameters.mutationRate)
+		keys.set(j, Search.r.nextFloat());
+            }
+            break;
+
+	default:
 			System.out.println("ERROR - No mutation method selected");
 		}
 	}
-	
+
 	public void randomSwap(){
 		// Randomly swap two genes:
 		int a = Search.r.nextInt(Parameters.numGenes);
@@ -125,8 +170,12 @@ public class Chromo
 		int val2 = chromo.get(b);
 		chromo.set(a, val2);
 		chromo.set(b, val1);
-		
+
 	}
+        
+        
+	
+
 
 /*******************************************************************************
 *                             STATIC METHODS                                   *
@@ -161,7 +210,7 @@ public class Chromo
 			 * Randomly select 2 parents; return the parent with the
 			 * higher fitness.
 			 */
-			
+
 			int p1 = Search.r.nextInt(Search.member.length);
 			double f1 = Search.member[p1].proFitness;
 			int p2 = Search.r.nextInt(Search.member.length);
@@ -179,16 +228,16 @@ public class Chromo
 				else
 					return p2;
 			}
-			
+
 		case 4:    // BJ: Rank Selection
-			
+
 			// Create a sorted list of fitnesses:
 			ArrayList<SortPair> to_sort = new ArrayList<SortPair>();
 			for (j=0; j < Parameters.popSize; ++j){
 				to_sort.add(new SortPair(Search.member[j].proFitness, j));
 			}
 			to_sort.sort(new MyComparator());
-			
+
 			// Create a Roulette wheel with slices based on rank for each member
 			ArrayList<Integer> rankWheel = new ArrayList<Integer>();
 			for (j=0; j < Parameters.popSize; ++j){
@@ -197,11 +246,11 @@ public class Chromo
 					rankWheel.add(to_sort.get(j).position);
 				}
 			}
-			
+
 			// Select a parent!
 			int p = Search.r.nextInt(rankWheel.size());
 			return rankWheel.get(p);
-			
+
 		default:
 			System.out.println("ERROR - No selection method selected");
 		}
@@ -221,13 +270,26 @@ public class Chromo
 			xoverPoint1 = 1 + (int)(Search.r.nextDouble() * (Parameters.numGenes * Parameters.geneSize-1));
 			Collections.copy(child1.chromo, parent1.chromo);
 			Collections.copy(child2.chromo, parent2.chromo);
-			sequenceCrossover(child1.chromo, child2.chromo, xoverPoint1);
+			crossLists(child1.chromo, child2.chromo, xoverPoint1);
+
+
 			break;
 
 		case 2:     //  Two Point Crossover
 			// TODO: To implement, just call crossLists() twice...
 
 		case 3:     //  Uniform Crossover
+
+
+		case 4:     //Random Key Single Point Crossover
+
+            xoverPoint1 = 1 + Search.r.nextInt(Parameters.geneSize-1);
+            Collections.copy(child1.keys, parent1.keys);
+			Collections.copy(child2.keys, parent2.keys);
+			crossLists(child1.keys, child2.keys, xoverPoint1);
+            child1.sortKeys();
+            child2.sortKeys();
+            break;
 
 		default:
 			System.out.println("ERROR - Bad crossover method selected");
@@ -249,6 +311,12 @@ public class Chromo
 		//  Create child chromosome from parental material
 		Collections.copy(child.chromo, parent.chromo);
 
+		//  Handles when keys are used
+		if(parent.keys != null)
+            Collections.copy(child.keys, parent.keys);
+
+
+
 		//  Set fitness values back to zero
 		child.rawFitness = -1;   //  Fitness not yet evaluated
 		child.sclFitness = -1;   //  Fitness not yet scaled
@@ -262,66 +330,26 @@ public class Chromo
 		targetA.chromo = sourceB.chromo;
 		Collections.copy(targetA.chromo, sourceB.chromo);
 
+		if(targetA.keys != null)
+        {
+            targetA.keys = sourceB.keys;
+            Collections.copy(targetA.keys, sourceB.keys);
+        }
+
 		targetA.rawFitness = sourceB.rawFitness;
 		targetA.sclFitness = sourceB.sclFitness;
 		targetA.proFitness = sourceB.proFitness;
 		return;
 	}
-	
+
 	// BJ: Perform 1-point cross-over on two lists *****************************
-	
+
 	public static <T> void crossLists(List<T> l1, List<T> l2, int crossPoint){
 		for (int i = 0; i < l1.size(); ++i){
 			if (i >= crossPoint){
 				T temp = l1.get(i);
 				l1.set(i, l2.get(i));
 				l2.set(i, temp);
-			}
-		}
-	}
-	
-	// BJ: Perform 1-point cross-over on two lists AND preserve sequences ******
-	
-	public static  void sequenceCrossover(List<Integer> l1, List<Integer> l2, int crossPoint){
-		HashSet<Integer> seen1 = new HashSet<Integer>();
-		HashSet<Integer> seen2 = new HashSet<Integer>();
-		HashMap<Integer, Integer> duplicates1 = new HashMap<Integer, Integer>();
-		HashMap<Integer, Integer> duplicates2 = new HashMap<Integer, Integer>();
-		// Blindly cross lists:
-		crossLists(l1, l2, crossPoint);
-		// Check for duplicates:
-		for (int i = 0; i < l1.size(); ++i){
-			if (seen1.contains(l1.get(i)))
-				duplicates1.put(l1.get(i), i);
-			else
-				seen1.add(l1.get(i));
-			if (seen2.contains(l2.get(i)))
-				duplicates2.put(l2.get(i), i);
-			else
-				seen2.add(l2.get(i));
-		}
-		// Replace duplicates (in l1):
-		for (int dup: duplicates1.keySet()){
-			// Find the first num not in seen1 in l2
-			for (int j = 0; j < l2.size(); ++j){
-				int in2 = l2.get(j);
-				if (!seen1.contains(in2)){
-					l1.set(duplicates1.get(dup), in2);
-					seen1.add(in2);
-					break;
-				}
-			}
-		}
-		// Replace duplicates (in l2):
-		for (int dup: duplicates2.keySet()){
-			// Find the first num not in seen1 in l2
-			for (int j = 0; j < l1.size(); ++j){
-				int in1 = l1.get(j);
-				if (!seen2.contains(in1)){
-					l2.set(duplicates2.get(dup), in1);
-					seen2.add(in1);
-					break;
-				}
 			}
 		}
 	}
@@ -333,19 +361,20 @@ public class Chromo
 class SortPair{
 	double fitness;
 	int position;
-	
+
 	public SortPair(double fitness, int position){
 		this.fitness = fitness;
 		this.position = position;
 	}
-	
+
 	public String toString(){
 		return fitness + ", " + position;
 	}
+
 }
 
 class MyComparator implements Comparator<SortPair> {
-    public int compare(SortPair a, SortPair b) { 
+    public int compare(SortPair a, SortPair b) {
           return Double.compare(a.fitness, b.fitness);
     }
 }
